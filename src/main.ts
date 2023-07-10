a2b173294bc98f83bb2d4e9cf2e68262d906eed5
ybadr99 <yousefbadr909@gmail.com>
2023-07-10 12:27:14 +0300

    Add init app and load data into the main

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,48 @@
+import './css/style.css';
+import TodoList from './model/TodoList';
 import TodoItem from './model/TodoItem';
-const todo = new TodoItem();
-todo.id = '123';
-todo.item = 'test';
-todo.checked = false;
+import ListTemplate from './template/ListTemplate';
 
-console.log(todo);
+const initApp = (): void => {
+  const todoList = TodoList.instance;
+  const template = ListTemplate.instance;
+
+  // Add listener to new entry form submit
+  const form = document.getElementById('itemEntryForm') as HTMLFormElement;
+
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    // Get the new item value validate
+    const input = document.getElementById('newItem') as HTMLInputElement;
+    const newValue: string = input.value.trim();
+    if (!newValue) return;
+
+    // calculate item ID
+    const itemId: number = todoList.list.length
+      ? +todoList.list[todoList.list.length - 1].id + 1
+      : 1;
+
+    // create new item
+    const newItem = new TodoItem(itemId.toString(), newValue);
+    // Add new item to full list
+    todoList.addItem(newItem);
+    // Re-render list with new item included
+    template.render(todoList);
+  });
+
+  // Add listener to "Clear" button
+  const clearButton = document.getElementById(
+    'clearItemsButton'
+  ) as HTMLButtonElement;
+
+  clearButton.addEventListener('click', () => {
+    todoList.clearList();
+    template.clear();
+  });
+  // load initial data
+  todoList.load();
+  // initial render of template
+  template.render(todoList);
+};
+
+document.addEventListener('DOMContentLoaded', initApp);
